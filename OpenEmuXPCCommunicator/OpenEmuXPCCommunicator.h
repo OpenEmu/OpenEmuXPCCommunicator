6d123baa038ef8ce9eb4593ee6b2a948fd2de5dd
Remy Demarest <remy.demarest@gmail.com>
2013-07-14 15:15:55 -0700

    Initial Commit

---
+++ OpenEmuXPCCommunicator/OpenEmuXPCCommunicator.h
@@ -0,0 +1,13 @@
+//
+//  OpenEmuXPCCommunicator.h
+//  OpenEmuXPCCommunicator
+//
+//  Created by Remy Demarest on 14/07/2013.
+//  Copyright (c) 2013 OpenEmu Team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenEmuXPCCommunicator : NSObject
+
+@end
---
+++ OpenEmuXPCCommunicator/OpenEmuXPCCommunicator.m
@@ -0,0 +1,13 @@
+//
+//  OpenEmuXPCCommunicator.m
+//  OpenEmuXPCCommunicator
+//
+//  Created by Remy Demarest on 14/07/2013.
+//  Copyright (c) 2013 OpenEmu Team. All rights reserved.
+//
+
+#import "OpenEmuXPCCommunicator.h"
+
+@implementation OpenEmuXPCCommunicator
+
+@end
